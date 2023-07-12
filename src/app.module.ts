42d598c981f127a75a50592f318ff0f9168b0f8b
Bohdan Onyshchenko <bohdan.onyshchenko@guesty.com>
2023-07-12 10:58:46 +0300

    fix dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,8 +5,11 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Block, BlockSchema } from './schema/block.schema';
 import { Listing, ListingSchema } from './schema/listing.schema';
+import { ConfigModule } from '@nestjs/config';
+
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGO_CONNECTION_STRING),
     MongooseModule.forFeature([
       { name: Block.name, schema: BlockSchema },
